31599944b8f97c9287bff3bf59bb2a43848dbda1
muhammed <muhammed.thasneem@yahoo.com>
2017-10-23 12:25:08 +0530

    Initial commit

--- DropBoxUrlDownload/app/src/main/java/com/cybercampuz/calibroz/dropboxurldownload/MainActivity.java
+++ DropBoxUrlDownload/app/src/main/java/com/cybercampuz/calibroz/dropboxurldownload/MainActivity.java
@@ -1,13 +1,38 @@
 package com.cybercampuz.calibroz.dropboxurldownload;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mVideoView = (VideoView) findViewById(R.id.videoView);
+        startDownload();
+    }
+
+    private void startDownload() {
+
+        mVideoView.setVideoURI(Uri.parse("https://www.dropbox.com/s/1j5ku254ydovzt1/one.mp4?dl=1"));
+        mVideoView.start();
+
+//        Ion.with(this)
+//                .load("https://www.dropbox.com/s/1j5ku254ydovzt1/one.mp4?dl=1")
+//                .write(new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "one.mp4"))
+//                .setCallback(new FutureCallback<File>() {
+//                    @Override
+//                    public void onCompleted(Exception e, File file) {
+//                        Log.d("FILE_DOWNLOAD", "onCompleted: " + file.getName());
+//                    }
+//                });
     }
 }
